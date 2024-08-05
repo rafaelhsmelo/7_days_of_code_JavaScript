4b345d46f324bf793c26d41a93ae0d7b1136c5f3
Rafael Melo <rafaelhsmelo@hotmail.com>
2024-08-05 19:55:03 -0300

    Versão final do desafio 03

--- day_03.js
+++ day_03.js
@@ -1,42 +1,64 @@
-// Seu desafio de hoje é criar os destinos possíveis de um jogo, em que o usuário consiga escolher:
-
-// 1. Se quer seguir para área de Front-End ou seguir para a área de Back-End.
 const areas = {
     front: ['react', 'vue'],
     back: ['c#', 'java']
-  };
-  
-  let estudos = prompt('Você gostaria de estudar Front-End ou Back-End?').toLowerCase();
-  
-  // Loop para definir se estudará Front ou Back-End.
-  while (true) {
+};
+
+let estudos = prompt('Você gostaria de estudar Front-End ou Back-End?').toLowerCase();
+
+// Loop para definir se estudará Front ou Back-End.
+while (true) {
     if (areas.hasOwnProperty(estudos)) {
-      break;
+        break;
     } else {
-      estudos = prompt('Descupe, não entendi. Você gostaria de estudar Front-End ou Back-End?').toLowerCase();
+        estudos = prompt('Descupe, não entendi. Você gostaria de estudar Front-End ou Back-End?').toLowerCase();
     }
-  }
-  
-  let linguagem = prompt(`Você gostaria de estudar ${areas[estudos].join(' ou ')}?`).toLowerCase();
-  
-  // Loop para definir a linguagem
-  while (true) {
+}
+
+let linguagem = prompt(`Você gostaria de estudar ${areas[estudos].join(' ou ')}?`).toLowerCase();
+
+// Loop para definir a linguagem
+while (true) {
     if (areas[estudos].includes(linguagem)) {
-      break;
+        break;
     } else {
-      linguagem = prompt(`Desculpe, não entendi. As opções para ${estudos} são ${areas[estudos].join(' ou ')}.`).toLowerCase();
+        linguagem = prompt(`Desculpe, não entendi. As opções para ${estudos} são ${areas[estudos].join(' ou ')}.`).toLowerCase();
     }
-  }
-  
-  console.log(`Você escolheu estudar ${estudos} com foco em ${linguagem}.`);
-  
+}
 
-// 2. Caso esteja na área de Front-End, se quer aprender React ou aprender Vue. Caso esteja na área de Back-End, poderá aprender C# ou aprender Java.
+let especialidade = prompt(`Você gostaria de ser especialista em ${linguagem} ou se tornar desenvolvedor Fullstack?`).toLowerCase();
 
-// 3. Depois, independente das escolhas anteriores, o usuário poderá escolher entre seguir se especializando na área escolhida ou seguir se desenvolvendo para se tornar Fullstack. Você deve exibir na tela uma mensagem específica para cada escolha.
+// Loop para definir se será Fullstack ou especilista.
+while (true) {
+    if (especialidade == linguagem || especialidade == 'especialista') {
+        alert(`Continue estudando ${linguagem} para ser um dev ${estudos}-end especialista!`);
+        break;
+    } else if (especialidade == 'fullstack') {
+        alert(`Para se tornar Fullstack, terá que aprender outras linguagens também!`);
+        estudos = 'Fullstack'
+        break;
+    } else {
+        especialidade = prompt(`Desculpe, não entendi. Você gostaria de se especialista em ${linguagem} ou se tornar desenvolvedor Fullstack?`).toLowerCase();
+    }
+}
 
-// 4. Por fim, pergunte quais são as tecnologias nas quais a pessoa gostaria de se especializar ou de conhecer. Aqui, a pessoa pode responder N tecnologias, uma de cada vez. Então, enquanto ela continuar respondendo ok para a pergunta: “Tem mais alguma tecnologia que você gostaria de aprender?”, continue apresentando para ela o Prompt, para que ela complete o nome da tecnologia em questão. E, logo depois, apresente uma mensagem comentando algo sobre a linguagem inserida.
+let listaDeTecnologias = [];
+alert(listaDeTecnologias.length);
+let tecnologia;
 
-// O importante é que a pessoa que estiver jogando possa sempre escolher qual decisão tomar para conseguir aprender e se desenvolver na área de programação.
+alert(listaDeTecnologias.length);
 
-// Além disso, também é essencial que, ao final do jogo, ela possa inserir quantas tecnologias quiser na lista de aprendizado.
+// Loop para adicionar tecnologias adicionais.
+while (tecnologia !== 'encerrar') {
+    tecnologia = prompt('Que outra tecnologia você gostaira de conhecer? Responda uma por vez ou "encerrar" para finalizar.');
+    if (tecnologia !== 'encerrar') {
+        listaDeTecnologias.push(tecnologia);
+    }
+}
+
+if (listaDeTecnologias.length > 1) {
+    alert(`Então você quer estudar ${listaDeTecnologias.join(', ')} além de  ${linguagem}! Que legal`);
+} else if (listaDeTecnologias.length === 1) {
+    alert(`Boa sorte na sua jornada para dev ${estudos} aprendendo ${listaDeTecnologias[0]}!`);
+} else {
+    alert(`Você escolheu focar apenas em ${linguagem}!`);
+}
