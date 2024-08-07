8cb7a749434c6c1201274470eafcf0974b4cf9a5
rafaelhsmelo <rafaelhsmelo@hotmail.com>
2024-08-07 12:30:50 -0300

    Versão final do quarto desafio.

--- day_04.js
+++ day_04.js
@@ -1,76 +1,73 @@
+// Variáveis globais.
 let numSecreto, totalTentativas, contadorTentativas;
-let numMax = 50;
-let botao = document.getElementById('botao');
+const numMax = 50;
+const botao = document.getElementById('botao');
 
-//Estado inicial do jogo
+// Inicializa o jogo.
 function iniciar() {
-    //Determina o número máximo e sorteia um aleatório.
+    //Sorteia o número aleatório aleatório.
     numSecreto = Math.round(Math.random() * numMax) + 1;
     console.log(`Shhh. O número secreto é ${numSecreto}. Mas não conta pra ninguém!`);
 
-    //Determina e avisa o número de chances.
+    //Determina e avisa o número de tentativas.
     totalTentativas = 10;
     contadorTentativas = 0;
     imprime('chances', `Você tem ${totalTentativas} tentativas.`);
+    imprime('msg','');
 }
 
-//Chama a função para iniciar o jogo.
+// Chama a função para iniciar o jogo.
 iniciar();
 
-//Imprime o 'txt' no 'id' informado dentro do HTML.
+// Atualiza o conteúdo HTML
 function imprime(id, txt) {
     document.getElementById(id).innerText = txt;
 }
 
-//Checa se no texto deve ser 'tentativa' ou 'tentativaS'.
+// Retorna a forma correta de 'tentativa'.
 function checaPluralTentativa() {
     return totalTentativas === 1 ? 'tentativa' : 'tentativas';
 }
 
-//Função executada quando o botão é clicado.
+// Função executada ao clicar no botão.
 function chutar() {
     //Recebe o valor digitado e limpa o campo para o próximo chute.
     let numChute = parseInt(document.getElementById('chute').value);
     document.getElementById('chute').value = '';
     document.getElementById('chute').focus();
 
-    //Checa se o chute é inválido.
-    if (isNaN(numChute) || numChute < 0) {
+    // Valida a entrada.
+    if (isNaN(numChute) || numChute < 0 || chutar > numMax) {
         imprime('msg', 'Chute inválido. Tente novamente.');
+        return;
+    }
+    // Processa o chute.
+    totalTentativas--;
+    contadorTentativas++;
 
-        //Checa se ainda há chances de tentar.
-    } else if (totalTentativas > 1) {
-        totalTentativas--;
-        contadorTentativas++;
+    //Checa se houve acerto ou erro.
+    if (numChute === numSecreto) {
+        imprime('msg', 'Parabéns! Você ganhou.');
+        imprime('chances', `Você acertou em ${contadorTentativas} ${checaPluralTentativa()}.`);
 
-        //Checa se houve acerto.
-        if (numChute === numSecreto) {
-            imprime('msg', 'Parabéns! Você ganhou.');
-            imprime('chances', `Você acertou em ${contadorTentativas} ${checaPluralTentativa()}.`);
-
-            // Muda o texto e função do botão para "Reiniciar"
-            botao.innerText = 'Reiniciar';
-            botao.onclick = reiniciar;
-
-            //Havendo erro, avisa.
-        } else {
-            //Checa e avisa se o chute é menor ou maior que o número secreto.
-            if (numSecreto < numChute) {
-                imprime('msg', `Que pena, você errou. O número sorteado é menor que ${numChute}`);
-                imprime('chances', `Você ainda tem ${totalTentativas} ${checaPluralTentativa()}.`);
-            } else {
-                imprime('msg', `Que pena, você errou. O número sorteado é maior que ${numChute}`);
-                imprime('chances', `Você ainda tem ${totalTentativas} ${checaPluralTentativa()}.`);
-            }
-        }
-        //Não havendo mais chances, avisa.
-    } else {
-        imprime('msg', 'Que pena. Você perdeu');
-        imprime('chances', `Acabaram suas tentativas, tente de novo.`);
-
-        // Muda o texto e função do botão para "Reiniciar"
         botao.innerText = 'Reiniciar';
         botao.onclick = reiniciar;
+
+    } else {
+        if (numChute > numSecreto) {
+            imprime('msg', `Que pena, você errou. O número sorteado é menor que ${numChute}`);
+        } else {
+            imprime('msg', `Que pena, você errou. O número sorteado é maior que ${numChute}`);
+        }
+
+        if (totalTentativas > 0) {
+            imprime('chances', `Você ainda tem ${totalTentativas} ${checaPluralTentativa()}.`);
+        } else {
+            imprime('msg', 'Que pena. Você perdeu.');
+            imprime('chances', 'Acabaram suas tentativas. Tente de novo.');
+            botao.innerText = 'Reiniciar';
+            botao.onclick = reiniciar;
+        }
     }
 }
 
