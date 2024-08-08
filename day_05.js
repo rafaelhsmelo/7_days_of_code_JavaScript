c58580e5a8e3fc558a74981986d505043f3d9599
rafaelhsmelo <rafaelhsmelo@hotmail.com>
2024-08-08 12:04:54 -0300

    Adicionado numeração em cada item de cada categoria e opção de cancelar o primeiro prompt.

--- day_05.js
+++ day_05.js
@@ -8,26 +8,58 @@ let listaHTML = document.getElementById('listaDeCompras');
 listaHTML.innerText = 'Sua lista está vazia.\n';
 
 function atualizaLista() {
-    if (frutas.length == 0 && laticinios.length == 0 && congelados.length == 0 && doces.length == 0) {
-        listaHTML.innerText = 'Sua lista está vazia.\n';
-    } else {
-        listaHTML.innerText = `Frutas: \n ${frutas.join('\n')}\n
-                               Lacticínios: \n ${laticinios.join('\n')}\n
-                               Congelados: \n ${congelados.join('\n')}\n
-                               Doces: \n ${doces.join('\n')}\n`
+    let listaFinal = '';
+
+    //Checa se cada categoria está vazia para imprimir na lista.
+    if (frutas.length > 0) {
+        listaFinal += 'Frutas:\n';
+        frutas.forEach((item, index) => {
+            listaFinal += `${index + 1}. ${item}\n`;
+        });
     }
+
+    if (laticinios.length > 0) {
+        listaFinal += 'Lacticínios:\n';
+        laticinios.forEach((item, index) => {
+            listaFinal += `${index + 1}. ${item}\n`;
+        });
+    }
+
+    if (congelados.length > 0) {
+        listaFinal += 'Congelados:\n';
+        congelados.forEach((item, index) => {
+            listaFinal += `${index + 1}. ${item}\n`;
+        });
+    }
+
+    if (doces.length > 0) {
+        listaFinal += 'Doces:\n';
+        doces.forEach((item, index) => {
+            listaFinal += `${index + 1}. ${item}\n`;
+        });
+    }
+
+    listaHTML.innerText = listaFinal || 'Sua lista está vazia.\n';
 }
+
 
 function adicionar() {
 
     while (true) {
         numCategoria = parseInt(prompt('Qual a categoria que gostaria de adicionar:\n 1) Frutas\n 2) Lacticínios\n 3) Congelados\n 4) Doces\n\n Difite apenas o número.'));
+
+        // Clicar 'cancelar' sai do loop.
+        if (numCategoria === num) {
+            return;
+        }
+
+        // Correlaciona número e categoria.
         switch (numCategoria) {
             case 1:
                 nomeCategoria = frutas;
                 break;
             case 2:
-                nomeCategoria = laticínios
+                nomeCategoria = laticinios
                 break;
             case 3:
                 nomeCategoria = congelados
@@ -42,15 +74,13 @@ function adicionar() {
         }
         break;
     }
-    console.log(nomeCategoria);
 
-    item = prompt(`Qual o item você gostaria de adicionar à categoria ${nomeCategoria}?`);
-
-    console.log(item);
-
-    nomeCategoria.push(item);
-
-    atualizaLista();
+    // Pede o item, o adiciona à categoria e atualiza a lista.
+    item = prompt(`Qual o item você gostaria de adicionar à categoria?`);
+    if (item) {
+        nomeCategoria.push(item);
+        atualizaLista();
+    }
 
 }
 
