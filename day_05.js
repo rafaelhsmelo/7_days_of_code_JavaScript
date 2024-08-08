e58520704a9c1459493104f58f3224871e92d5f8
rafaelhsmelo <rafaelhsmelo@hotmail.com>
2024-08-08 11:12:15 -0300

    Adicionado função de atualizar e limpar lista.

--- day_05.js
+++ day_05.js
@@ -3,23 +3,37 @@ let laticinios = [];
 let congelados = [];
 let doces = [];
 
+let nomeCategoria, numCategoria, item;
+let listaHTML = document.getElementById('listaDeCompras');
+listaHTML.innerText = 'Sua lista está vazia.\n';
+
+function atualizaLista() {
+    if (frutas.length == 0 && laticinios.length == 0 && congelados.length == 0 && doces.length == 0) {
+        listaHTML.innerText = 'Sua lista está vazia.\n';
+    } else {
+        listaHTML.innerText = `Frutas: \n ${frutas.join('\n')}\n
+                               Lacticínios: \n ${laticinios.join('\n')}\n
+                               Congelados: \n ${congelados.join('\n')}\n
+                               Doces: \n ${doces.join('\n')}\n`
+    }
+}
+
 function adicionar() {
 
     while (true) {
-        let numCategoria = parseInt(prompt('Qual a categoria que gostaria de adicionar:\n 1) Frutas\n 2) Lacticínios\n 3) Congelados\n 4) Doces\n\n Difite apenas o número.'));
-        let nomeCategoria;
+        numCategoria = parseInt(prompt('Qual a categoria que gostaria de adicionar:\n 1) Frutas\n 2) Lacticínios\n 3) Congelados\n 4) Doces\n\n Difite apenas o número.'));
         switch (numCategoria) {
             case 1:
-                nomeCategoria = 'Frutas'
+                nomeCategoria = frutas;
                 break;
             case 2:
-                nomeCategoria = 'Laticínios'
+                nomeCategoria = laticínios
                 break;
             case 3:
-                nomeCategoria = 'Congelados'
+                nomeCategoria = congelados
                 break;
             case 4:
-                nomeCategoria = 'Doces'
+                nomeCategoria = doces
                 break;
 
             default:
@@ -28,4 +42,22 @@ function adicionar() {
         }
         break;
     }
+    console.log(nomeCategoria);
+
+    item = prompt(`Qual o item você gostaria de adicionar à categoria ${nomeCategoria}?`);
+
+    console.log(item);
+
+    nomeCategoria.push(item);
+
+    atualizaLista();
+
+}
+
+function limpar() {
+    frutas = [];
+    laticinios = [];
+    congelados = [];
+    doces = [];
+    atualizaLista();
 }
